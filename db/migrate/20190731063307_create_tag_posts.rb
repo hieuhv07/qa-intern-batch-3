7f4df8191f5e147fb14879fbc0e04cacfb0c4f20
HieuCR7 <hieuhv97@gmail.com>
2019-07-31 16:14:11 +0700

    create migration

--- db/migrate/20190731063307_create_tag_posts.rb
+++ db/migrate/20190731063307_create_tag_posts.rb
@@ -1,6 +1,8 @@
 class CreateTagPosts < ActiveRecord::Migration[5.2]
   def change
     create_table :tag_posts do |t|
+    	t.references :post, index: true, foreign_key: true
+    	t.references :tag, index: true, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20190731063327_create_comments.rb
+++ db/migrate/20190731063327_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
+    	t.text :content
+    	t.references :user, index: true, foreign_key: true
+    	t.references :post, index: true, foreign_key: true
 
       t.timestamps
     end
